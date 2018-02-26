49ea541a373afcb08f328a7b092413e63c024739
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-02-26 15:14:02 -0700

    Registered Order model with admin

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import MenuItem, SupplyItem
+from .models import MenuItem, SupplyItem, Order
+
 # Register your models here.
 admin.site.register(MenuItem)
 admin.site.register(SupplyItem)
+admin.site.register(Order)
