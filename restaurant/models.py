14368d65e22e360cadaff1123e0a2e38798e17f7
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-02-26 15:07:46 -0700

    Made docstrings more pythonic in models.py

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,27 +1,26 @@
 from django.db import models
 
-# TODO: Register this in the database, make the migrations, and start playing with the API and admin side to test.
-"""
-SupplyItem class/Model:
-===================================
-Contains the following members/fields:
- - name: CharField containing the name of the item.
- - units: CharField with the name of the units this item is measured in, i.e. oz, c, gal, box, etc...
- - quantity: FloatField containing the amount of units left in stock.
-"""
-
 
 class SupplyItem(models.Model):
+    # TODO: Register this in the database, make the migrations, and start playing with the API and admin side to test.
+    """
+    SupplyItem class/Model:
+    ===================================
+    Contains the following members/fields:
+    - name: CharField containing the name of the item.
+    - units: CharField with the name of the units this item is measured in, i.e. oz, c, gal, box, etc...
+    - quantity: FloatField containing the amount of units left in stock.
+    """
 
-    """Returns the name of the SupplyItem."""
     def __str__(self):
+        """Returns the name of the SupplyItem."""
         return self.name
 
-    """
-    Method that's called when a MenuItem requiring this Supply is called.
-    If there is sufficient quantity then it decrements, otherwise it raises a descriptive error.
-    """
     def decrement(self, amt):
+        """
+        Method that's called when a MenuItem requiring this Supply is called.
+        If there is sufficient quantity then it decrements, otherwise it raises a descriptive error.
+        """
         if amt >= self.quantity:
             self.quantity -= amt
         else:
@@ -34,26 +33,24 @@ class SupplyItem(models.Model):
     quantity = models.FloatField()
 
 
-"""
-MenuItem class/model:
-===================================
-Contains the following members/fields:
- - available: True or False, depending on if all necessary Inventory items are available.
- - name: CharField containing the name of the dish.
- - ingredients: ManyToManyField relationship that links each MenuItem with the corresponding Inventory items.
- - price: a DecimalField that holds the dish's price, fixed at two decimal places.
- - description: TextField for containing a short description of the dish.
- - image: An ImageField for storing an image of the dish.
-    (NOTE): Inherits from FileField, which has some nuances in the documentation that I need to research before implementing.
- - 
-"""
-
-
 class MenuItem(models.Model):
+    """
+    MenuItem class/model:
+    ===================================
+    Contains the following members/fields:
+     - available: True or False, depending on if all necessary Inventory items are available.
+     - name: CharField containing the name of the dish.
+     - ingredients: ManyToManyField relationship that links each MenuItem with the corresponding Inventory items.
+     - price: a DecimalField that holds the dish's price, fixed at two decimal places.
+     - description: TextField for containing a short description of the dish.
+     - image: An ImageField for storing an image of the dish.
+        (NOTE): Inherits from FileField, which has some nuances in the documentation that I need to research before implementing.
+     -
+    """
 
-    """Returns a formatted string containing the name, price, and description of the item."""
     def __str__(self):
-        return "%s - $%s \n\t%s" %(self.name, str(self.price), self.description)
+        """Returns a formatted string containing the name, price, and description of the item."""
+        return "%s - $%s \n\t%s" % (self.name, str(self.price), self.description)
 
     # TODO: Implement some try-catch functionality for trying to prepare something that's unavailable
     def prepare_item(self):
