ef1f5b78b9c683db2604807c548d352e819ffa78
Justin Fairbourn <jfairbourn96@bitbucket.org>
2018-02-26 00:31:45 -0700

    Got most of the MenuItem/SupplyItem functionality, aside from everything marked with TODO. Still need to do the Menu and Order.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,82 @@
 from django.db import models
 
-# Create your models here.
+# TODO: Register this in the database, make the migrations, and start playing with the API and admin side to test.
+"""
+SupplyItem class/Model:
+===================================
+Contains the following members/fields:
+ - name: CharField containing the name of the item.
+ - units: CharField with the name of the units this item is measured in, i.e. oz, c, gal, box, etc...
+ - quantity: FloatField containing the amount of units left in stock.
+"""
+
+
+class SupplyItem(models.Model):
+
+    """Returns the name of the SupplyItem."""
+    def __str__(self):
+        return self.name
+
+    """
+    Method that's called when a MenuItem requiring this Supply is called.
+    If there is sufficient quantity then it decrements, otherwise it raises a descriptive error.
+    """
+    def decrement(self, amt):
+        if amt >= self.quantity:
+            self.quantity -= amt
+        else:
+            raise ValueError("Insufficient quantity of %s.\n\t Need: %s %s, Have: %s %s"
+                             % (self.name, str(amt), self.units, str(self.quantity), self.units))
+
+    # member variables
+    name = models.CharField(max_length=30)
+    units = models.CharField(max_length=10)
+    quantity = models.FloatField()
+
+
+"""
+MenuItem class/model:
+===================================
+Contains the following members/fields:
+ - available: True or False, depending on if all necessary Inventory items are available.
+ - name: CharField containing the name of the dish.
+ - ingredients: ManyToManyField relationship that links each MenuItem with the corresponding Inventory items.
+ - price: a DecimalField that holds the dish's price, fixed at two decimal places.
+ - description: TextField for containing a short description of the dish.
+ - image: An ImageField for storing an image of the dish.
+    (NOTE): Inherits from FileField, which has some nuances in the documentation that I need to research before implementing.
+ - 
+"""
+
+
+class MenuItem(models.Model):
+
+    """Returns a formatted string containing the name, price, and description of the item."""
+    def __str__(self):
+        return "%s - $%s \n\t%s" %(self.name, str(self.price), self.description)
+
+    # TODO: Implement some try-catch functionality for trying to prepare something that's unavailable
+    def prepare_item(self):
+        pass
+        # for supply in supplies try/catch supply.decrement
+
+    available = models.BooleanField(True)
+    name = models.CharField(max_length=30)
+    # TODO: Figure out how to properly store the SupplyItems along with the quantity for each item.
+    #  Maybe create a KeyVal class as the following stackoverflow suggests?
+    #  https://stackoverflow.com/questions/402217/how-to-store-a-dictionary-on-a-django-model
+    ingredients = models.ManyToManyField(SupplyItem, on_delete=models.PROTECT)
+    price = models.DecimalField(max_digits=None, decimal_places=2)
+    description = models.TextField()
+    # TODO: Read the documentation and figure out how the ImageField works.
+    # image = models.ImageField(upload_to=None, height_field=None, width_field=None, max_length=100)
+
+
+# TODO: Implement the Menu class/model
+class Menu(models.Model):
+    pass
+
+
+# TODO: Implement the Order class/model
+class Order(models.Model):
+    pass
