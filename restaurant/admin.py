2a4a9a7a2ace354ef700e1aade4f65c1be27782e
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-02-26 16:35:15 -0700

    Tweaked admin page to allow OrderItems to be modified directly (and only) from the Order admin page

--- restaurant/admin.py
+++ restaurant/admin.py
@@ -2,8 +2,21 @@ from django.contrib import admin
 
 from .models import MenuItem, SupplyItem, Order, OrderItem
 
+
+class OrderItemInline(admin.StackedInline):
+    model = OrderItem
+    extra = 1
+
+
+class OrderAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None, {'fields': ['pin']}),
+    ]
+
+    inlines = [OrderItemInline]
+
+
 # Register your models here.
 admin.site.register(MenuItem)
 admin.site.register(SupplyItem)
-admin.site.register(Order)
-admin.site.register(OrderItem)
+admin.site.register(Order, OrderAdmin)
