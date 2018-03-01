8129937d638b49f61100808bbaf494780c460b32
GSmily-PC\GSmily <gavinbrowning@aggiemail.usu.edu>
2018-03-01 01:07:13 -0700

    Fixed extra '/' warning

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     # ex: /restaurant/
     path('', views.index, name='index'),
     # ex: /restaurant/customerMenu/
-    path('/customerMenu/', views.customerMenu, name='customerMenu'),
+    path('customerMenu/', views.customerMenu, name='customerMenu'),
     # ex: /restaurant/customerOrder/
-    path('/customerOrder/', views.customerOrder, name='customerOrder'),
+    path('customerOrder/', views.customerOrder, name='customerOrder'),
 ]
