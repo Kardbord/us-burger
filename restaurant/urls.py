c47e3a36cd9a30e7c1de2a6106d87e65a4064fe6
GSmily-PC\GSmily <gavinbrowning@aggiemail.usu.edu>
2018-03-01 00:57:13 -0700

    Linked all HTML to views.py and urls.py

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -2,6 +2,12 @@ from django.urls import path
 
 from . import views
 
+app_name = 'restaurant'
 urlpatterns = [
+    # ex: /restaurant/
     path('', views.index, name='index'),
+    # ex: /restaurant/customerMenu/
+    path('/customerMenu/', views.customerMenu, name='customerMenu'),
+    # ex: /restaurant/customerOrder/
+    path('/customerOrder/', views.customerOrder, name='customerOrder'),
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render, HttpResponse
 
-def index(request):
+from .models import MenuItem
 
+def index(request):
     return render(request, 'restaurant/index.html')
+
+def customerMenu(request):
+    return render(request, 'restaurant/customerMenu.html')
+
+def customerOrder(request):
+    return render(request, 'restaurant/customerOrder.html')
