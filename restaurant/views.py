90062ec813f72348a82ecb42d2fd8fe4be2a6382
Justin Fairbourn <jfairbourn96@bitbucket.org>
2018-03-01 23:30:53 -0700

    I think I have all of the views/templates configured to where "/restaurant/cutomerMenu/" displays all of the available menu items. Also added the SupplyItem instances to the database so the availability and preparation works form the API.

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,12 +1,20 @@
-from django.shortcuts import render, HttpResponse
+from django.http import HttpResponse
+from django.template import loader
+from django.shortcuts import render
 
 from .models import MenuItem
+
 
 def index(request):
     return render(request, 'restaurant/index.html')
 
 def customerMenu(request):
-    return render(request, 'restaurant/customerMenu.html')
+    latest_menu = MenuItem.objects.filter(available=True)
+    template = loader.get_template('restaurant/customerMenu.html')
+    context = {
+        'latest_menu': latest_menu
+    }
+    return HttpResponse(template.render(context, request))
 
 def customerOrder(request):
     return render(request, 'restaurant/customerOrder.html')
