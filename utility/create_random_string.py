f0a41a706875f8036db1ffb5574f8cd3e10b9feb
GSmily-PC\GSmily <gavinbrowning@aggiemail.usu.edu>
2018-03-19 00:45:07 -0600

    Added random string function

---
+++ utility/create_random_string.py
@@ -0,0 +1,20 @@
+import random
+import string
+
+
+def create_random_string(digit_weight=20, string_length=5):
+    random_string = ''
+
+    for x in range(string_length):
+        random_int = random.randint(1, 100)
+
+        if random_int <= digit_weight:
+            random_string += str(random.randint(1, 9))
+        else:
+            random_string += random.choice(string.ascii_letters)
+
+    return random_string
+
+
+randoms = [create_random_string(string_length=10) for x in range(5)]
+print(randoms)
