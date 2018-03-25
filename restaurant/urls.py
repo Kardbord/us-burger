37bafea9b417f808c17d39d79e33b15047f435c6
Tanner <tanner.kvarfordt@aggiemail.usu.edu>
2018-03-25 16:31:35 -0600

    Did some prep-work for creating a dynamic order page

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -8,6 +8,10 @@ urlpatterns = [
     path('', views.index, name='index'),
     # ex: /restaurant/customerMenu/
     path('customerMenu/', views.customerMenu, name='customerMenu'),
+
+    # ex: /restaurant/customerOrder/1
+    # path('customerOrder/<int:order_pk>/', views.customerOrder, name='customerOrder'),
+    # TODO: delete the line below and uncomment the line above once customerMenu sends an order_pk as an arg to customerOrder
     # ex: /restaurant/customerOrder/
-    path('customerOrder/', views.customerOrder, name='customerOrder'),
+    path('customerOrder/', views.customerOrder, name='customerOrder')
 ]
--- restaurant/views.py
+++ restaurant/views.py
@@ -1,8 +1,8 @@
 from django.http import HttpResponse
 from django.template import loader
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
-from .models import MenuItem, WaitTime
+from .models import MenuItem, WaitTime, Order
 
 
 def index(request):
@@ -24,6 +24,15 @@ def customerMenu(request):
     return HttpResponse(template.render(context, request))
 
 
+# TODO: once the customerMenu sends an order_pk, replace function def with the line below
+#def customerOrder(request, order_pk):
+#    wait_time = WaitTime.objects.last()
+#    order = get_object_or_404(Order, pk=order_pk)
+#    context = {
+#        'order': order,
+#        'wait_time': wait_time
+#    }
+#    return render(request, 'restaurant/customerOrder.html', context)
 def customerOrder(request):
     wait_time = WaitTime.objects.last()
     context = {
