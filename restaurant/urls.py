5aaf8e414fa5af3ee18a19f53873b90bb91c3c4f
tannerwheeler <tanner.wheeler@aggiemail.usu.edu>
2018-03-30 22:46:01 -0600

    Fixed resolutions to merge conflicts

--- restaurant/urls.py
+++ restaurant/urls.py
@@ -12,5 +12,10 @@ urlpatterns = [
     path('newOrder/', views.newOrder, name='newOrder'),
     # ex: /restaurant/customerOrder/1
     path('customerOrder/<int:order_pk>/', views.customerOrder, name='customerOrder'),
-
+	# this will verify if an order exists
+	path('verify/', views.verify, name='verify'),
+	# ex: /init/
+	# This url is for testing/developing purposes ONLY
+	# TODO: delete this in production version
+	path('init/', views.init, name='init')
 ]
