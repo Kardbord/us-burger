04d3a168a3c802ec5da1c5196939d23c76e298a5
Kardbord <tanner.kvarfordt@gmail.com>
2018-04-21 20:12:01 -0600

    Added django-heroku as a dependency

--- us_burger/settings.py
+++ us_burger/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 import os
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+import django_heroku
+
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 STATIC_ROOT = os.path.join(BASE_DIR, 'restaurant/static')
 STATIC_URL = '/static/'
@@ -124,3 +126,5 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+django_heroku.settings(locals())
