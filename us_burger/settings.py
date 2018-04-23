6087492f4c84dea2379d9ef437fc55b15fd6b629
Kardbord <tanner.kvarfordt@gmail.com>
2018-04-23 12:32:52 -0600

    Turned on Debug mode

--- us_burger/settings.py
+++ us_burger/settings.py
@@ -26,7 +26,7 @@ STATIC_URL = '/static/'
 SECRET_KEY = '-1*vp_uo(^@^dpyu4om@urht)i&dse!4+zb%&pn&$*pml5r@^5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 SECURE_CONTENT_TYPE_NOSNIFF = True
 SECURE_BROWSER_XSS_FILTER = True
