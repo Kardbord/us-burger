58b333cc82b342a313bfa35e5e376017eb0f8152
Kardbord <tanner.kvarfordt@gmail.com>
2018-04-23 16:51:00 -0600

    Added whitenoise back into STATICFILES_STORAGE

--- us_burger/settings.py
+++ us_burger/settings.py
@@ -142,4 +142,4 @@ django_heroku.settings(locals())
 
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
-# STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
